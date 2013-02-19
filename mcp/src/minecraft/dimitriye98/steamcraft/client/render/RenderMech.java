62f3d05ae85ce2446693fa0c56172f05b28e8da3
Dimitriye Danilovic <Dimitriye98@Dimitriyes-MacBook-Pro.local>
2013-02-19 01:02:56 -0800

    Annoing eclipse auto-indent, ready for immediate revert

--- mcp/src/minecraft/dimitriye98/steamcraft/client/render/RenderMech.java
+++ mcp/src/minecraft/dimitriye98/steamcraft/client/render/RenderMech.java
@@ -13,56 +13,57 @@ import cpw.mods.fml.relauncher.SideOnly;
 import dimitriye98.steamcraft.entity.EntityMech;
 
 @SideOnly(Side.CLIENT)
-public class RenderMech extends Render
-{
-    /** instance of ModelMech for rendering */
-    protected ModelBase modelMech;
+public class RenderMech extends Render {
+	/** instance of ModelMech for rendering */
+	protected ModelBase modelMech;
 
-    public RenderMech()
-    {
-        this.shadowSize = 0.5F;
-        this.modelMech = new ModelBoat();
-    }
+	public RenderMech() {
+		this.shadowSize = 0.5F;
+		this.modelMech = new ModelBoat();
+	}
 
-    /**
-     * The render method used in RenderMech that renders the Mech model.
-     */
-    public void renderMech(EntityMech par1EntityMech, double par2, double par4, double par6, float par8, float par9)
-    {
-        GL11.glPushMatrix();
-        GL11.glTranslatef((float)par2, (float)par4, (float)par6);
-        GL11.glRotatef(180.0F - par8, 0.0F, 1.0F, 0.0F);
-        float var10 = (float)par1EntityMech.getTimeSinceHit() - par9;
-        float var11 = (float)par1EntityMech.getDamageTaken() - par9;
+	/**
+	 * The render method used in RenderMech that renders the Mech model.
+	 */
+	public void renderMech(EntityMech par1EntityMech, double par2, double par4,
+			double par6, float par8, float par9) {
+		GL11.glPushMatrix();
+		GL11.glTranslatef((float) par2, (float) par4, (float) par6);
+		GL11.glRotatef(180.0F - par8, 0.0F, 1.0F, 0.0F);
+		float var10 = par1EntityMech.getTimeSinceHit() - par9;
+		float var11 = par1EntityMech.getDamageTaken() - par9;
 
-        if (var11 < 0.0F)
-        {
-            var11 = 0.0F;
-        }
+		if (var11 < 0.0F) {
+			var11 = 0.0F;
+		}
 
-        if (var10 > 0.0F)
-        {
-            GL11.glRotatef(MathHelper.sin(var10) * var10 * var11 / 10.0F * (float)par1EntityMech.rotationYaw, 1.0F, 0.0F, 0.0F);
-        }
+		if (var10 > 0.0F) {
+			GL11.glRotatef(MathHelper.sin(var10) * var10 * var11 / 10.0F
+					* par1EntityMech.rotationYaw, 1.0F, 0.0F, 0.0F);
+		}
 
-        this.loadTexture("/terrain.png");
-        float var12 = 0.75F;
-        GL11.glScalef(var12, var12, var12);
-        GL11.glScalef(1.0F / var12, 1.0F / var12, 1.0F / var12);
-        this.loadTexture("/item/boat.png");
-        GL11.glScalef(-1.0F, -1.0F, 1.0F);
-        this.modelMech.render(par1EntityMech, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F, 0.0625F);
-        GL11.glPopMatrix();
-    }
+		this.loadTexture("/terrain.png");
+		float var12 = 0.75F;
+		GL11.glScalef(var12, var12, var12);
+		GL11.glScalef(1.0F / var12, 1.0F / var12, 1.0F / var12);
+		this.loadTexture("/item/boat.png");
+		GL11.glScalef(-1.0F, -1.0F, 1.0F);
+		this.modelMech.render(par1EntityMech, 0.0F, 0.0F, -0.1F, 0.0F, 0.0F,
+				0.0625F);
+		GL11.glPopMatrix();
+	}
 
-    /**
-     * Actually renders the given argument. This is a synthetic bridge method, always casting down its argument and then
-     * handing it off to a worker function which does the actual work. In all probabilty, the class Render is generic
-     * (Render<T extends Entity) and this method has signature public void doRender(T entity, double d, double d1,
-     * double d2, float f, float f1). But JAD is pre 1.5 so doesn't do that.
-     */
-    public void doRender(Entity par1Entity, double par2, double par4, double par6, float par8, float par9)
-    {
-        this.renderMech((EntityMech)par1Entity, par2, par4, par6, par8, par9);
-    }
+	/**
+	 * Actually renders the given argument. This is a synthetic bridge method,
+	 * always casting down its argument and then handing it off to a worker
+	 * function which does the actual work. In all probabilty, the class Render
+	 * is generic (Render<T extends Entity) and this method has signature public
+	 * void doRender(T entity, double d, double d1, double d2, float f, float
+	 * f1). But JAD is pre 1.5 so doesn't do that.
+	 */
+	@Override
+	public void doRender(Entity par1Entity, double par2, double par4,
+			double par6, float par8, float par9) {
+		this.renderMech((EntityMech) par1Entity, par2, par4, par6, par8, par9);
+	}
 }
