37c0c14d0032660b41a655471da64ce6363a2dac
Dimitriye Danilovic <Dimitriye98@Dimitriyes-MacBook-Pro.local>
2013-02-23 11:56:10 -0800

    Revert "Revert "Revert "Reverting, sorry"""
    This reverts commit 4abea72b1d3bc38cd3b104d7512d0c93f508ac52.

--- mcp/src/minecraft/steamcraft/steamcraft/client/ClientProxy.java
+++ mcp/src/minecraft/steamcraft/steamcraft/client/ClientProxy.java
@@ -1,16 +1,16 @@
-package dimitriye98.steamcraft.client;
+package steamcraft.steamcraft.client;
 
+import steamcraft.steamcraft.SteamCraft;
+import steamcraft.steamcraft.client.render.RenderBoiler;
+import steamcraft.steamcraft.client.render.RenderMech;
+import steamcraft.steamcraft.client.render.ResearchTableRender;
+import steamcraft.steamcraft.common.CommonProxy;
+import steamcraft.steamcraft.entity.EntityMech;
+import steamcraft.steamcraft.entity.EntityMusketBall;
+import steamcraft.steamcraft.tileentity.TileEntityResearchTable;
 import net.minecraftforge.client.MinecraftForgeClient;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import cpw.mods.fml.client.registry.RenderingRegistry;
-import dimitriye98.steamcraft.client.render.RenderBoiler;
-import dimitriye98.steamcraft.client.render.RenderMech;
-import dimitriye98.steamcraft.client.render.ResearchTableRender;
-import dimitriye98.steamcraft.common.CommonProxy;
-import dimitriye98.steamcraft.common.SteamCraft;
-import dimitriye98.steamcraft.entity.EntityMech;
-import dimitriye98.steamcraft.entity.EntityMusketBall;
-import dimitriye98.steamcraft.tileentity.TileEntityResearchTable;
 public class ClientProxy extends CommonProxy
 {
     @Override
@@ -21,7 +21,5 @@ public class ClientProxy extends CommonProxy
         RenderingRegistry.registerEntityRenderingHandler(EntityMusketBall.class, new RenderBoiler(4));
         RenderingRegistry.registerEntityRenderingHandler(EntityMech.class, new RenderMech());
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityResearchTable.class, new ResearchTableRender());
-
     }
-
 }
