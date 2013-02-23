e356c248c2d5d16eedbfe27a6f6c1306fe947c31
Dimitriye Danilovic <Dimitriye98@Dimitriyes-MacBook-Pro.local>
2013-02-23 11:42:12 -0800

    Added required overrides

--- mcp/src/minecraft/steamcraft/steamcraft/tileentity/TileEntitySteamPipe.java
+++ mcp/src/minecraft/steamcraft/steamcraft/tileentity/TileEntitySteamPipe.java
@@ -1,11 +1,32 @@
 package steamcraft.steamcraft.tileentity;
 
-import java.util.ArrayList;
-
 import net.minecraft.tileentity.TileEntity;
+import steamcraft.steamcraft.api.ISteamTank;
 
-public class TileEntitySteamPipe extends TileEntity {
+public class TileEntitySteamPipe extends TileEntity implements ISteamTank {
 
+	@Override
+	public int getSteam() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
+	@Override
+	public int getCapacity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int fill(int amount, boolean virtual) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int drain(int amount, boolean virtual) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
